fbe57678a5738d780e5e194fbf5843f9b5af1253
Erik Aschenbrenner <ashburnere@gmail.com>
2019-01-09 20:30:40 +0100

    add code for mdule 1

---
+++ python-for-data-science/1-1-Types.py
@@ -0,0 +1,43 @@
+# Try your first Python output
+print('Hello, Python!')
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+# Practice on writing comments
+print('Hello, Python!') # This line prints a string
+# print('Hi')
+
+# Integer
+print(type(11))
+
+# Float
+print(type(2.14))
+
+# String
+print(type("Hello, Python 101!"))
+
+# Convert 2 to a float
+float(2)
+
+# Casting 1.1 to integer will result in loss of information
+int(1.1)
+
+# Convert a string into an integer
+int('1')
+
+# Convert a string into an integer with error
+#int('1 or 2 people')
+
+# Convert the string "1.2" into a float
+float('1.2')
+
+# Convert an integer to a string
+str(1)
+
+# Type of True
+print(type(True))
+
+# Type of False
+print(type(False))
